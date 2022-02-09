30e97a63b0db3f28e9cecbc57ad19e5f10f256ab
Muhammad Ramadhan <m.ramadhan@hotmail.co.id>
2022-02-09 21:33:22 +0700

    29. Final Function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -8,8 +8,17 @@ class SocialMedia
 // Bila class ini diberi final, maka class FakeFacebook akan error
 class Facebook extends SocialMedia
 {
+    // Bila function ini diberi final, maka function yg di override di class FakeFacebook akan error
+    public function login(string $username, string $password): bool
+    {
+        return true;
+    }
 }
 
 class FakeFacebook extends Facebook
 {
+    public function login(string $username, string $password): bool
+    {
+        return true;
+    }
 }
