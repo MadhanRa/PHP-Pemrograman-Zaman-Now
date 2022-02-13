22a7c06d0fa2cfd46719172679134eaff13757ba
Muhammad Ramadhan <m.ramadhan@hotmail.co.id>
2022-02-13 12:55:23 +0700

    37. Magic Function

--- data/Student.php
+++ data/Student.php
@@ -16,4 +16,25 @@ class Student
     {
         unset($this->sample);
     }
+
+    public function __toString(): string
+    {
+        return "Student id: $this->id, name: $this->name, value: $this->value";
+    }
+
+    public function __invoke(...$arguments): void
+    {
+        $join =  join(", ", $arguments);
+        echo "Invoke student with argumets $join" . PHP_EOL;
+    }
+
+    public function __debugInfo()
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "value" => $this->value,
+            "sample" => $this->sample,
+        ];
+    }
 }
---
+++ data/ToString.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "data/Student.php";
+
+$student1 = new Student();
+$student1->id = "1";
+$student1->name = "Budi";
+$student1->value = 100;
+
+$string = (string) $student1;
+echo $string . PHP_EOL;
+
+// Bisa juga seperti ini
+echo $student1 . PHP_EOL;
